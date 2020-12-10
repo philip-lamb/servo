6ac147233cf98a5e4f13e8ccb729746f4c01e1a9
Philip Lamb <phil.git@eden.net.nz>
2020-12-11 12:59:09 +1300

    Include VS environment in build script.

--- build_ss2.bat
+++ build_ss2.bat
@@ -1,4 +1,8 @@
-mach build -d --libsimpleservo2
-copy target\debug\simpleservo2.h ..\servo-unity\src\ServoUnityPlugin
-copy target\debug\simpleservo2.dll.lib ..\servo-unity\src\ServoUnityPlugin\Windows
-copy target\debug\simpleservo2.dll ..\servo-unity\src\ServoUnity\Assets\Plugins\x64
+if "%VSCMD_VER%" EQU "" (
+    call "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+call mach build -d --libsimpleservo2
+
+xcopy /f /y target\debug\simpleservo2.h ..\servo-unity\src\ServoUnityPlugin
+xcopy /f /y target\debug\simpleservo2.dll.lib ..\servo-unity\src\ServoUnityPlugin\Windows
+xcopy /f /y target\debug\simpleservo2.dll ..\servo-unity\src\ServoUnity\Assets\Plugins\x64
