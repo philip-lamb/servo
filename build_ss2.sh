6b1846eb6a92e07f24c7d2f54fb190f228f72c21
Philip Lamb <phil.git@eden.net.nz>
2021-02-10 10:39:10 +1300

    Mac build script command-line handling.

--- build_ss2.sh
+++ build_ss2.sh
@@ -3,19 +3,58 @@
 # Get our location.
 OURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+CONFIG=release
+
+function usage {
+    echo "Usage: $(basename $0) [-|--help] [--debug]"
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    usage
+fi
+
 # -e = exit on errors
 set -e
 
 # -x = debug
 set -x
 
-./mach build -d --libsimpleservo2
+# Parse parameters
+while test $# -gt 0
+do
+    case "$1" in
+        --install-unity-editor-dependencies) INSTALL_UNITY_EDITOR_DEPS=1
+            ;;
+        --debug) CONFIG=debug
+            ;;
+        --help) usage
+            ;;
+        -h) usage
+            ;;
+        --*) echo "bad option $1"
+            usage
+            ;;
+        *) echo "bad argument $1"
+            usage
+            ;;
+    esac
+    shift
+done
 
-cp -pf target/debug/simpleservo2.h ../servo-unity/src/ServoUnityPlugin
-cp -pf target/debug/libsimpleservo2.dylib ../servo-unity/src/ServoUnity/Assets/Plugins
+if [ $CONFIG = "release" ] ; then
+    MACH_CONFIG=--release
+else
+    MACH_CONFIG=--dev
+fi
+
+./mach build $MACH_CONFIG --libsimpleservo2
+
+cp -pf target/$CONFIG/simpleservo2.h ../servo-unity/src/ServoUnityPlugin
+cp -pf target/$CONFIG/libsimpleservo2.dylib ../servo-unity/src/ServoUnity/Assets/Plugins
 
 # Copy plugins
-(cd target/debug &&
+(cd target/$CONFIG &&
 cp -pf libgstapp.dylib \
 libgstaudiobuffersplit.dylib \
 libgstaudioconvert.dylib \
@@ -55,43 +94,45 @@ libgstosxvideo.dylib \
 libgstnice.dylib \
 ../../../servo-unity/src/ServoUnity/Assets/Plugins)
 
-# Also copy to Unity app folder to allow to run in Editor.
-(cd target/debug &&
-cp -pf libgstapp.dylib \
-libgstaudiobuffersplit.dylib \
-libgstaudioconvert.dylib \
-libgstaudiofx.dylib \
-libgstaudioparsers.dylib \
-libgstaudioresample.dylib \
-libgstautodetect.dylib \
-libgstcoreelements.dylib \
-libgstdeinterlace.dylib \
-libgstdtls.dylib \
-libgstgio.dylib \
-libgstid3tag.dylib \
-libgstid3demux.dylib \
-libgstinterleave.dylib \
-libgstisomp4.dylib \
-libgstlibav.dylib \
-libgstmatroska.dylib \
-libgstogg.dylib \
-libgstopengl.dylib \
-libgstopus.dylib \
-libgstplayback.dylib \
-libgstproxy.dylib \
-libgstrtp.dylib \
-libgstrtpmanager.dylib \
-libgsttheora.dylib \
-libgsttypefindfunctions.dylib \
-libgstvideoconvert.dylib \
-libgstvideofilter.dylib \
-libgstvideoparsersbad.dylib \
-libgstvideoscale.dylib \
-libgstvorbis.dylib \
-libgstvolume.dylib \
-libgstvpx.dylib \
-libgstwebrtc.dylib \
-libgstosxaudio.dylib \
-libgstosxvideo.dylib \
-libgstnice.dylib \
-/Applications/Unity/Hub/Editor/2019.3.13f1/Unity.app/Contents/MacOS)
+if [ $INSTALL_UNITY_EDITOR_DEPS ] ; then
+    # Also copy to Unity app folder to allow to run in Editor.
+    (cd target/debug &&
+    cp -pf libgstapp.dylib \
+    libgstaudiobuffersplit.dylib \
+    libgstaudioconvert.dylib \
+    libgstaudiofx.dylib \
+    libgstaudioparsers.dylib \
+    libgstaudioresample.dylib \
+    libgstautodetect.dylib \
+    libgstcoreelements.dylib \
+    libgstdeinterlace.dylib \
+    libgstdtls.dylib \
+    libgstgio.dylib \
+    libgstid3tag.dylib \
+    libgstid3demux.dylib \
+    libgstinterleave.dylib \
+    libgstisomp4.dylib \
+    libgstlibav.dylib \
+    libgstmatroska.dylib \
+    libgstogg.dylib \
+    libgstopengl.dylib \
+    libgstopus.dylib \
+    libgstplayback.dylib \
+    libgstproxy.dylib \
+    libgstrtp.dylib \
+    libgstrtpmanager.dylib \
+    libgsttheora.dylib \
+    libgsttypefindfunctions.dylib \
+    libgstvideoconvert.dylib \
+    libgstvideofilter.dylib \
+    libgstvideoparsersbad.dylib \
+    libgstvideoscale.dylib \
+    libgstvorbis.dylib \
+    libgstvolume.dylib \
+    libgstvpx.dylib \
+    libgstwebrtc.dylib \
+    libgstosxaudio.dylib \
+    libgstosxvideo.dylib \
+    libgstnice.dylib \
+    /Applications/Unity/Hub/Editor/2020.2.0f1/Unity.app/Contents/MacOS)
+fi
